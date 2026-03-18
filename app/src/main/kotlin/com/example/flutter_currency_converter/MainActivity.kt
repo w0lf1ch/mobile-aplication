f61669b1376c04c263abae27aaa648e02d71bc79
al1AM <dan23ivanenko@gmail.com>
2026-03-18 10:45:53 +0200

    save local changes

---
+++ app/src/main/kotlin/com/example/flutter_currency_converter/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.flutter_currency_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+// Главная Android Activity для Flutter.
+// Используем НОВЫЙ v2 embedding.
+// Старый import io.flutter.app.FlutterActivity больше нельзя использовать.
+class MainActivity : FlutterActivity()
